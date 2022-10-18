6adf4e13fee6711ba0aff3e6b3d820e703f7239e
Mazin Fouad <m.fouad@live.de>
2022-10-18 20:29:51 +0200

    added animation to cloud

--- models/cloud.class.js
+++ models/cloud.class.js
@@ -1,9 +1,17 @@
 class Cloud extends MovableObject {
   y = 30;
   width = 520;
-  height = 240;
+  height = 300;
   constructor() {
     super().loadImage('../img/5_background/layers/4_clouds/1.png');
-    this.x = 200 + Math.random() * 500;
+
+    this.x = Math.random() * 500;
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      this.x -= 0.15;
+    }, 1000 / 60);
   }
 }
