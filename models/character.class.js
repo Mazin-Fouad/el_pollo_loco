b44884c1b4d122dba678a4d345fc4553e19eda99
Mazin Fouad <m.fouad@live.de>
2022-10-19 19:09:44 +0200

    added animation to character

--- models/character.class.js
+++ models/character.class.js
@@ -2,16 +2,30 @@ class Character extends MovableObject {
   y = 140;
   height = 295;
   width = 110;
+  IMAGES_WALKING = [
+    'img/2_character_pepe/2_walk/W-21.png',
+    'img/2_character_pepe/2_walk/W-22.png',
+    'img/2_character_pepe/2_walk/W-23.png',
+    'img/2_character_pepe/2_walk/W-24.png',
+    'img/2_character_pepe/2_walk/W-25.png',
+    'img/2_character_pepe/2_walk/W-26.png',
+  ];
+  currentImage = 0;
+
   constructor() {
     super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
-    this.loadImages([
-      'img/2_character_pepe/2_walk/W-21.png',
-      'img/2_character_pepe/2_walk/W-22.png',
-      'img/2_character_pepe/2_walk/W-23.png',
-      'img/2_character_pepe/2_walk/W-24.png',
-      'img/2_character_pepe/2_walk/W-25.png',
-      'img/2_character_pepe/2_walk/W-26.png',
-    ]);
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length;
+      //0,1,2,3,4,5,0,1,2..
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 200);
   }
 
   jump() {}
