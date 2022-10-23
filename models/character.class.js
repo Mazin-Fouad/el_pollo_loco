85ae82e6ee939be17efaf837c55d0ae95af4dfb4
Mazin Fouad <m.fouad@live.de>
2022-10-23 12:16:53 +0200

    added keyboard to the game

--- models/character.class.js
+++ models/character.class.js
@@ -10,6 +10,7 @@ class Character extends MovableObject {
     'img/2_character_pepe/2_walk/W-25.png',
     'img/2_character_pepe/2_walk/W-26.png',
   ];
+  world;
 
   constructor() {
     super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
@@ -19,11 +20,13 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length; //0,1,2,3,4,5,0,1,2..
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
-    }, 200);
+      if (this.world.keyboard.RIGHT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length; //0,1,2,3,4,5,0,1,2..
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 100);
   }
 
   jump() {}
