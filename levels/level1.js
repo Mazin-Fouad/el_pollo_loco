37c1d0928f32e7396d7292397d41ec7e252dc894
Mazin Fouad <m.fouad@live.de>
2022-10-26 20:09:52 +0200

    added yellow chicken

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,5 @@
 const level1 = new Level(
-  [new Chicken(), new Chicken(), new Chicken(), new Endboss()],
+  [new Chicken(), new Chicken(), new Chicken(), new YellowChicken(), new YellowChicken(), new YellowChicken(), new YellowChicken(), new Endboss()],
 
   [
     new BackgroundObject('../img/5_background/layers/air.png', -719),
---
+++ models/yellow-chicken.class.js
@@ -0,0 +1,21 @@
+class YellowChicken extends MovableObject {
+  y = 350;
+  height = 80;
+  width = 70;
+  IMAGES_WALKING = ['../img/3_enemies_chicken/chicken_small/1_walk/1_w.png', '../img/3_enemies_chicken/chicken_small/1_walk/2_w.png', '../img/3_enemies_chicken/chicken_small/1_walk/3_w.png'];
+
+  constructor() {
+    super().loadImage(this.IMAGES_WALKING[0]);
+    this.loadImages(this.IMAGES_WALKING);
+    this.x = 800 + Math.random() * 700;
+    this.speed = 0.15 + Math.random() * 0.25;
+    this.animate();
+  }
+
+  animate() {
+    this.moveLeft();
+    setInterval(() => {
+      this.playAnimation(this.IMAGES_WALKING);
+    }, 200);
+  }
+}
