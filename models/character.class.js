e844832f31fd895d8c8f6c86ff4b413905b5e059
Mazin Fouad <m.fouad@live.de>
2022-10-28 15:03:18 +0200

    added jump animation to character

--- models/character.class.js
+++ models/character.class.js
@@ -11,12 +11,24 @@ class Character extends MovableObject {
     'img/2_character_pepe/2_walk/W-25.png',
     'img/2_character_pepe/2_walk/W-26.png',
   ];
+  IMAGES_JUMPING = [
+    '../img/2_character_pepe/3_jump/J-31.png',
+    '../img/2_character_pepe/3_jump/J-32.png',
+    '../img/2_character_pepe/3_jump/J-33.png',
+    '../img/2_character_pepe/3_jump/J-34.png',
+    '../img/2_character_pepe/3_jump/J-35.png',
+    '../img/2_character_pepe/3_jump/J-36.png',
+    '../img/2_character_pepe/3_jump/J-37.png',
+    '../img/2_character_pepe/3_jump/J-38.png',
+    '../img/2_character_pepe/3_jump/J-39.png',
+  ];
   world;
   WALKING_SOUND = new Audio('../audio/character-walking.mp3');
 
   constructor() {
     super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.animate();
     this.applyGravity();
   }
@@ -39,9 +51,14 @@ class Character extends MovableObject {
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        //Walk animation
-        this.playAnimation(this.IMAGES_WALKING);
+      if (this.isAboveGround()) {
+        //Play Jump animation
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+          //play walk animation
+          this.playAnimation(this.IMAGES_WALKING);
+        }
       }
     }, 50);
   }
