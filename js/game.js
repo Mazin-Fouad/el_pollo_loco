c4e13d27be3a109629e70e781d136851185fa408
Mazin Fouad <m.fouad@live.de>
2022-12-20 19:27:59 +0100

    $*

--- js/game.js
+++ js/game.js
@@ -38,7 +38,9 @@ function addElPolloLoco() {
     <div class="mobilePanels">
           <div id="panelHolder1" class="panel-holder d-none">
             <button id="left" class="mobile-btn"><img src="img/icons/left-arrow .png" /></button>
-            <button id="right" class="mobile-btn"><img src="img/icons/arrow-point-to-right.png" /></button>
+            <button id="throw" class="mobile-btn"><img src="img/icons/forward.png" /></button>
+
+            <!-- <button id="right" class="mobile-btn"><img src="img/icons/arrow-point-to-right.png" /></button> -->
           </div>
 
           <div class="panel-holder">
@@ -48,7 +50,8 @@ function addElPolloLoco() {
 
           <div id="panelHolder2" class="panel-holder d-none">
             <button id="up" class="mobile-btn"><img src="img/icons/up-arrow .png" /></button>
-            <button id="throw" class="mobile-btn"><img src="img/icons/forward.png" /></button>
+            <!-- <button id="throw" class="mobile-btn"><img src="img/icons/forward.png" /></button> -->
+            <button id="right" class="mobile-btn"><img src="img/icons/arrow-point-to-right.png" /></button>
           </div>
          
         </div>
